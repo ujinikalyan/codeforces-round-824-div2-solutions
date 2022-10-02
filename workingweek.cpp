531a1f077f8a8e10516f3d83abf53cb4b1d2581b
Kalyan Gandhi ujini <ujinikalyangandhi@gmail.com>
2022-10-02 21:07:46 +0530

    Create workingweek.cpp

---
+++ workingweek.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+int main()
+{
+    //freopen("a.txt","r",stdin);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<n/3-2<<endl;
+        
+    }
+    return 0;
+}
